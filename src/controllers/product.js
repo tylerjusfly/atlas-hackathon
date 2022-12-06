bd4dd1b78f5e20df5d78e200e7c5e6beb27ecabd
farukade <farukadekunle@gmail.com>
2022-12-06 21:15:29 +0100

    category added

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product.model");
 const Category = require("../models/category.model");
 const { handleBadRequest, successHandler, handleError } = require("./constants");
 const { generateFakeProducts } = require("../utils/utils");
+const { log } = console;
 
 exports.createProduct = async (req, res) => {
   const { name, description, price, unit, category } = req.body;
@@ -47,9 +48,47 @@ exports.injectRandomProducts = async (req, res) => {
 
 exports.findProduct = async (req, res) => {
   try {
-    const { id, page } = req.query;
+    const { id, page, category, categoryId } = req.query;
     const limit = +req.query.limit ? +req.query.limit : 10;
     const offset = +page ? (+page - 1) * limit : 0;
+
+    if (categoryId && categoryId != "") {
+      let currentCat = await Category.findOne({ _id: categoryId });
+      if (!currentCat) return handleBadRequest(res, "category not found", 400);
+
+      let result = await Product.find({ category: categoryId }).limit(limit);
+      let total = await Product.find({ category: categoryId });
+      total = total.length;
+
+      if (result.length) {
+        const paging = {
+          page,
+          limit,
+          totalPages: Math.ceil(total / limit),
+          totalItems: total,
+        };
+        return successHandler(res, "found", result, false, paging);
+      }
+    }
+
+    if (category && category == "1") {
+      let categories = await Category.find();
+      if (!categories) {
+        return handleBadRequest(res, "categories not found", 400);
+      }
+
+      let result = {};
+      for (const item of categories) {
+        const catName = item.name;
+        let products = await Product.find({ categoryId: item._id }).limit(limit).exec();
+        result[catName] = products;
+      }
+      if (result) {
+        return successHandler(res, "found", result);
+      } else {
+        return handleBadRequest(res, "not found", 400);
+      }
+    }
 
     if (id && id !== "") {
       let result = await Product.findOne({ _id: id });
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory } = require("../controllers/category");
+const { createCategory, getCategories } = require("../controllers/category");
 
 /* GET home page. */
 router.post("/", createCategory);
+
+/* GET home page. */
+router.get("/", getCategories);
 
 module.exports = router;
