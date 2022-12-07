58f171fb769e5eb839f4a040f3fdb14e842506bf
tylerjusfly <tylerjusfly1@gmail.com>
2022-12-07 10:52:52 +0100

    random images

---
+++ src/utils/randomImages.js
@@ -0,0 +1,9 @@
+const images = [
+  "https://media.theeverygirl.com/wp-content/uploads/2022/06/the-everygirl-summer-clothes-gallery.jpg",
+  "https://m.media-amazon.com/images/I/71YkeCRKyTL._CR3,0,1914,1080_SR684,386_.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/d/dd/Bacon.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/4/4b/Cookies.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/0/0f/Serbian_bread.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/a/a1/Cake.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/9/99/Pumpkins.jpg",
+];
